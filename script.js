c4cbfded0abbeed35358c49ec6ab526bb6e16f3d
Kleverson Kenji Iwatani <96200706+iwataniKenji@users.noreply.github.com>
2022-02-17 13:10:24 -0300

    adding reset function

--- script.js
+++ script.js
@@ -303,6 +303,11 @@ class App {
     this.#workouts.forEach(work => {
       this._renderWorkout(work); // renderiza todos os workouts guardados
     })
+
+    reset() {
+      localStorage.removeItem('workouts');
+      location.reload();
+    }
   }
 }
 
